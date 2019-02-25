8d62561ee6b6decef1fcc4b076a9f65aa0faf2dd
Lirette <clirette@dxc.com>
2019-02-25 16:12:35 -0600

    Added modal for course delete

--- public/js/completed-courses.js
+++ public/js/completed-courses.js
@@ -23,3 +23,32 @@ links.forEach(link => link.addEventListener('click', (e) => {
     })
     .catch(err => console.error(err));
 }))
+
+let execDelete;
+const deleteModals = [...document.querySelectorAll('.delete-modal')];
+deleteModals.forEach(modal => modal.addEventListener('click', (e) => {
+    e.preventDefault();
+    const courseId = e.target.dataset.courseid;
+    const options = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        },
+        body: `id=${courseId}`
+    }
+    execDelete = () => {
+        fetch(e.target.dataset.href, options)
+        .then(res => {
+            return res.json()
+        })
+        .then(myJson => {
+            window.location = myJson.redirectUrl;
+        })
+        .catch(err => console.error(err));
+    };
+}))
+
+const deleteConfirms = [...document.querySelectorAll('.delete-confirm')];
+deleteConfirms.forEach(deleteConfirm => deleteConfirm.addEventListener('click', (e) => {
+    execDelete();
+}));
