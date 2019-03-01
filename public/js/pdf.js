46026036f781068402058a6f1c45ce1d69bc7935
Chase Lirette <chaselirette@yahoo.com>
2019-02-28 23:31:10 -0600

    Pull in DOM content for writing to PDF

--- public/js/pdf.js
+++ public/js/pdf.js
@@ -1,8 +1,12 @@
 const doc = new jsPDF();
 
+const firstName = document.getElementById('firstName');
+
 const createPDF = (e) => {
   doc.text('Hello World', 10, 10)
+  doc.text(firstName.value, 10, 20);
   doc.save('example.pdf');
+
 }
 
 const pdfButton = document.getElementById('pdf');
