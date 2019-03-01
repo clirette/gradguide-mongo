d71efc0b5bcd7cfaca7099fa8d98a33bcca7d562
Lirette <clirette@dxc.com>
2019-03-01 10:01:43 -0600

    Initial pdfmake commit.

--- public/js/pdf.js
+++ public/js/pdf.js
@@ -1,15 +1,15 @@
-const doc = new jsPDF();
-
 const firstName = document.getElementById('firstName');
+const pdfButton = document.getElementById('pdf');
 
 const createPDF = (e) => {
-  doc.text('Hello World', 10, 10)
-  doc.text(firstName.value, 10, 20);
-  doc.save('example.pdf');
+  fetch(`/api/student/${pdfButton.dataset.userid}`)
+  .then(response => response.json())
+  .then(resJSON => {
+    console.log(resJSON);
+  }).catch(err => console.error(err))
 
 }
 
-const pdfButton = document.getElementById('pdf');
 pdfButton.addEventListener('click', createPDF);
 
 
